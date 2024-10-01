a07d67cab4dd85a29b79835b37d01fb5079c4373
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-10-01 18:31:54 +0500

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,26 @@ const montserrat = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: "O'zagrosug'urta",
-  description: "O'zagrosug'urta aksiyadorlik jamiyati",
+  metadataBase: new URL("https://diyordemy.uz"),
+  title: "O'zagrosug'urta aksiyadorlik jamiyati",
+  description:
+    "O‘zagrosug‘urta aksiyadorlik jamiyati 1997 yil 25 fevralda O‘zbekiston Respublikasi Prezidentining 1713-sonli farmoniga asosan tashkil etilgan.",
+  authors: [{ name: "Diyorbek Sulaymonov", url: "https://diyor-dev.uz" }],
+  openGraph: {
+    title: "O'zagrosug'urta aksiyadorlik jamiyati",
+    description:
+      "O‘zagrosug‘urta aksiyadorlik jamiyati 1997 yil 25 fevralda O‘zbekiston Respublikasi Prezidentining 1713-sonli farmoniga asosan tashkil etilgan.",
+    type: "website",
+    url: "https://agros-clone.vercel.app/",
+    locale: "uz_UZ",
+    images:
+      "https://agros-clone.vercel.app/_next/image?url=%2Flogo.png&w=640&q=75",
+    countryName: "Uzbekistan",
+    siteName: "Agros",
+    emails: "diyorbeksulaymonov70@gmail.com",
+  },
   icons: { icon: "/icon.jpeg" },
+  keywords: "agros, agrosug'urta, uzagrosug'urta",
 };
 
 export default function RootLayout({ children }: ChildProps) {
